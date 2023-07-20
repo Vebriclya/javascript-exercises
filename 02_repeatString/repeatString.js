3052df1a69ced6ad90b3eaeca117ec1806a265c0
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-20 14:56:46 +0100

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    if(timesToRepeat < 0){
+        return "ERROR";
+    }
+    let result = "";
 
+    for(let i = 0; i < timesToRepeat; i++){
+        result += stringToRepeat;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
