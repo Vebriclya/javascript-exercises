e174b5fa7dc68dcee8abf3c1c51071d1901ab327
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-20 17:11:47 +0100

    Got the first test to pass, but fucked up the others, going to work on it tomorrow

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(enteredArray = [], firstItemToRemove, secondItemToRemove = "") {
+
+    let firstItemPosition = enteredArray.indexOf(firstItemToRemove);
+    let secondItemPosition = enteredArray.indexOf(secondItemToRemove);
+
+    enteredArray.splice(firstItemPosition, 1);
+    if(secondItemToRemove != ""){
+        enteredArray.splice(secondItemPosition, 1);
+    }
+
+    return enteredArray;
 
 };
 
