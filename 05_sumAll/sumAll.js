7bfd64da97528a6c30daa52eb635e284fd73773f
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 15:42:40 +0100

    Exercise 05: 4/6 tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,23 +2,28 @@ const sumAll = function (firstNumber, secondNumber) {
   let numbersToAdd = [];
   let largerNumber;
   let smallerNumber;
+  let result;
 
-  if (firstNumber > secondNumber) {
+  if (firstNumber < 0 || secondNumber < 0) {
+    result = "ERROR";
+  } else if (firstNumber > secondNumber) {
     largerNumber = firstNumber;
     smallerNumber = secondNumber;
-  } else {
+  } else if (firstNumber < secondNumber) {
     smallerNumber = firstNumber;
     largerNumber = secondNumber;
   }
 
-  for (let i = smallerNumber; i <= largerNumber; i++) {
-    numbersToAdd.push(i);
-  }
+  if (result != "ERROR") {
+    for (let i = smallerNumber; i <= largerNumber; i++) {
+      numbersToAdd.push(i);
+    }
 
-  let result = 0;
-  numbersToAdd.forEach((item) => {
-    result += item;
-  });
+    result = 0;
+    numbersToAdd.forEach((item) => {
+      result += item;
+    });
+  }
 
   return result;
 };
