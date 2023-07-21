64d7c9b13f19f087cd35ee2bca049932fd2a7768
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 17:07:32 +0100

    07: 3/6 tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
 const convertToCelsius = function (temperature) {
   // Take temperature
   // subtract 32 from tempterature
-  // divide by 1.8
+  // Multiply the result by 5
+  // Divide the result by 9
+
+  temperature -= 32;
+  temperature *= 5;
+  temperature /= 9;
+
+  temperature = Math.round(temperature * 10) / 10;
+
+  return temperature;
 };
 
 const convertToFahrenheit = function () {};
