8d4c41e47d79f0ee086682d1dc815ec39d43a21e
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 14:33:20 +0100

    Passed the first 2 tests of exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,20 @@
-const sumAll = function(firstNumber, secondNumber) {
-    // count up from first number until it is less than the second number
-    // store all those numbers in an array
-    // add them all together
+const sumAll = function (firstNumber, secondNumber) {
+  // count up from first number until it is less than the second number
+  // store all those numbers in an array
+  // add them all together
 
-    let numbersToAdd = [];
+  let numbersToAdd = [];
 
-    for(let i = firstNumber; i < secondNumber; i++){
-        numbersToAdd.push(i);
-    }
+  for (let i = firstNumber; i <= secondNumber; i++) {
+    numbersToAdd.push(i);
+  }
 
-    return numbersToAdd;
+  let result = 0;
+  numbersToAdd.forEach((item) => {
+    result += item;
+  });
+
+  return result;
 };
 
 // Do not edit below this line
