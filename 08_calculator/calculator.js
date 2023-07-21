b10c9cc1db90d4fcd81a3884b2c57db93a7a4287
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 17:13:43 +0100

    08: Initial commit and pass of 4/12 tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,20 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  num1 += num2;
+  return num1;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  num1 -= num2;
+  return num1;
 };
 
-const sum = function() {
-	
-};
+const sum = function () {};
 
-const multiply = function() {
+const multiply = function () {};
 
-};
+const power = function () {};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +23,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
