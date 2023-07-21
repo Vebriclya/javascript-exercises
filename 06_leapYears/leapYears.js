5b76ed61665d25a6f56a77836ec8d0e9394cab50
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 16:17:52 +0100

    Exercise 06: 6/6 tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -6,7 +6,7 @@ const leapYears = function (year) {
   if (year % 4 === 0) {
     result = true;
   }
-  if (year % 100 === 0) {
+  if (year % 100 === 0 && year % 400 != 0) {
     result = false;
   }
 
