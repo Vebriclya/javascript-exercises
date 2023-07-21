c9bb659aec40f8ee77b4ead1b71ba4244b054809
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 16:18:53 +0100

    Removed comments

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,4 @@
 const leapYears = function (year) {
-  //> Leap years are years divisible by four (like 1984 and 2004)
-  //Years divisible by 100 are not leap years (such as 1800 and 1900) unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years)
   let result = false;
 
   if (year % 4 === 0) {
