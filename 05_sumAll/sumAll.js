148acfe8b638c131d956e2e73b94f351310a3990
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-21 14:24:33 +0100

    Initial commit for exercise 05, added my first algorithm and working on finding out why it isn't correct

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    // count up from first number until it is less than the second number
+    // store all those numbers in an array
+    // add them all together
 
+    let numbersToAdd = [];
+
+    for(let i = firstNumber; i < secondNumber; i++){
+        numbersToAdd.push(i);
+    }
+
+    return numbersToAdd;
 };
 
 // Do not edit below this line
