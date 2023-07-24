75faaec07169ce7826def810a28c2b9afd3d1d2c
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-24 09:18:57 +0100

    09: Initial commit and rough outline

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (stringToCheck) {
+  // remove the spaces from the string, resave to same variable
+  // string to array
+  // reverse the array
+  // array back to (new) string
+  // compare new string to old string
+  // if they're the same, then comparision = true
+  // if not, comparison = false
 };
 
 // Do not edit below this line
