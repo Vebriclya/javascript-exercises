136ad4710eface626edbe965cd44f7f3d56504a5
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-24 14:38:49 +0100

    11: 1/1 test passed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,25 +1,8 @@
 const getTheTitles = function (bookArray) {
-  /* 
-You are given an array of objects that represent books with an author and a title that looks like this:
-
-```javascript
-const books = [
-  {
-    title: 'Book',
-    author: 'Name'
-  },
-  {
-    title: 'Book2',
-    author: 'Name2'
-  }
-]
-```
-
-Your job is to write a function that takes the array and returns an array of titles:
-
-```javascript
-getTheTitles(books) // ['Book','Book2']
-``` */
+  let titleArray = bookArray.map((book) => {
+    return book.title;
+  });
+  return titleArray;
 };
 
 // Do not edit below this line
