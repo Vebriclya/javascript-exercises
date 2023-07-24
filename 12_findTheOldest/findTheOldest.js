d4133961b7e6f69b1480ba9f811dcc1abc490059
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-24 16:55:45 +0100

    I'm in hell

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (peopleArray) {
+  const currentYear = new Date().getFullYear();
 
+  peopleArray.forEach((person) => {
+    if (!person.yearOfDeath) {
+      person.age = currentYear - person.yearOfBirth;
+    } else {
+      person.age = person.yearOfDeath - person.yearOfBirth;
+    }
+  });
+
+  peopleArray.sort((a, b) => {
+    return a.age - b.age;
+  });
+
+  return peopleArray.shift().name;
 };
 
 // Do not edit below this line
