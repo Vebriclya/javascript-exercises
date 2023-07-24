992923348cdf07624e472de938f0562d0f207cf5
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-24 17:27:29 +0100

    Multiple spelling error cockups later, 3/3 tests passed. I hated this one

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,10 +1,15 @@
 const findTheOldest = function (peopleArray) {
   const currentYear = new Date().getFullYear();
 
+  // See comment at the end of the HTML to see what this looked like prior to refactoring
   peopleArray.sort((a, b) => {
-    let ageA = a.yearOfBirth - a.yearOfDeath || currentYear - a.yearOfDeath;
-    let ageB = b.yearOfBirth - b.yearOfDeath || currentYear - b.yearOfDeath;
-    return ageA - ageB;
+    let ageA = a.yearOfDeath
+      ? a.yearOfDeath - a.yearOfBirth
+      : currentYear - a.yearOfBirth;
+    let ageB = b.yearOfDeath
+      ? b.yearOfDeath - b.yearOfBirth
+      : currentYear - b.yearOfBirth;
+    return ageB - ageA;
   });
 
   return peopleArray[0];
