8f948a7a0d53c25993085c49bc74b95cf94e4917
Vebriclya <135071199+Vebriclya@users.noreply.github.com>
2023-07-24 14:03:55 +0100

    10: Added test for minus numbers, all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,17 +1,26 @@
 const fibonacci = function (enteredNumber) {
-  let fibonacciArray = [1, 1];
-  let position1 = 0;
-  let position2 = 1;
+  let result;
 
-  for (let i = 1; i < enteredNumber; i++) {
-    let position3 = fibonacciArray[position1] + fibonacciArray[position2];
-    fibonacciArray.push(position3);
-    position1++;
-    position2++;
+  if (enteredNumber < 0) {
+    result = "OOPS";
+  } else {
+    let fibonacciArray = [1, 1];
+    let position1 = 0;
+    let position2 = 1;
+
+    for (let i = 1; i < enteredNumber; i++) {
+      let position3 = fibonacciArray[position1] + fibonacciArray[position2];
+      fibonacciArray.push(position3);
+      position1++;
+      position2++;
+    }
+
+    enteredNumber -= 1;
+
+    result = fibonacciArray[enteredNumber];
   }
 
-  enteredNumber -= 1;
-  return fibonacciArray[enteredNumber];
+  return result;
 };
 
 // Do not edit below this line
